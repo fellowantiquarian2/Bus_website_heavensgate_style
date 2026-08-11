b699e13256a5e4d5694d9585390d81a47efbaf6f
fellowantiquarian2 <opearsonla@gmail.com>
2026-08-11 18:37:23 -0400

    Update map-location.js

--- map-location.js
+++ map-location.js
@@ -4,7 +4,11 @@
 // window.BOOK_BUS_LOCATION = "131 Avenue A, New York, NY 10009";
 // window.BOOK_BUS_LOCATION = "40.7268,-73.9818";
 
+
+
 window.BOOK_BUS_LOCATION = "181 thompson st, New York, NY";
 window.BOOK_BUS_MAP_ZOOM = 16;
 
+window.BOOK_BUS_IS_OPEN = false;
+window.BOOK_BUS_LOCATION = "131 Avenue A, New York, NY 10009";
 window.BOOK_BUS_MAP_TYPE = "k"; // satellite view
