0927d83b560eff1905a60a937a3e85a2bc8e429c
fellowantiquarian2 <opearsonla@gmail.com>
2026-08-21 18:39:07 -0400

    Update Book Bus location

--- map-location.js
+++ map-location.js
@@ -4,7 +4,7 @@
 // window.BOOK_BUS_LOCATION = "131 Avenue A, New York, NY 10009";
 // window.BOOK_BUS_LOCATION = "40.7268,-73.9818";
 
-window.BOOK_BUS_LOCATION = "323 grand st. \nnew york, ny";
+window.BOOK_BUS_LOCATION = "162 Ave B, New york";
 window.BOOK_BUS_MAP_ZOOM = 18;
 window.BOOK_BUS_IS_OPEN = false;
 window.BOOK_BUS_MAP_TYPE = "k"; // satellite view
